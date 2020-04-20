747593288733c69a146ff0b4ab99266928ef818d
chris <cgisala@msn.com>
2020-04-19 22:32:38 -0500

    Encountered errors still.  Failed 2 test.

--- wishlist/travel_wishlist/models.py
+++ wishlist/travel_wishlist/models.py
@@ -15,6 +15,25 @@ class Place(models.Model):
     date_visited = models.DateField(blank=True, null=True)
     photo = models.ImageField(upload_to='user_images/', blank=True, null=True)
 
+    def save(self, *args, **kwargs):
+        #get reference to previous version of this Place
+        old_place = Place.objects.filter(pk=self.pk).first()
+        if old_place and old_place.photo:
+            if old_place.photo != self.photo:
+                self.delete_poto(old_place.photo)
+        
+        super().save(*args, **kwargs)
+
+    def delete_photo(self, photo):
+        if default_storage.exists(photo.name):
+            default_storage.delete(photo.name)
+    
+    def delete(self, *args, **kwargs):
+        if self.photo:
+            self.delete_photo(self.photo)
+
+        super().delete(*args, **kwargs)
+
     def __str__(self):
         photo_str = self.photo.url if self.photo else 'no photo'
         return f'{self.pk}: {self.name} visited? {self.visited} on {self.date_visited}\nPhoto {photo_str}'
